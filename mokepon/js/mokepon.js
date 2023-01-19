c6b7efa242bf96f0b021c86ef1608f821ce09733
Rosana Porras <rosanaporras98@gmail.com>
2023-01-19 09:54:45 +0100

    movimiento hacia todas las direcciones

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -43,6 +43,7 @@ let victoriasEnemigo = 0
 let vidasJugador = 3
 let vidasEnemigo = 3
 let lienzo = mapa.getContext("2d")
+let intervalo
 
 class Mokepon {
     constructor(nombre, foto, vida) {
@@ -50,6 +51,14 @@ class Mokepon {
         this.foto = foto
         this.vida = vida 
         this.ataques = []
+        this.x = 20
+        this.y = 30
+        this.ancho = 80
+        this.alto = 80
+        this.mapaFoto = new Image()
+        this.mapaFoto.src = foto
+        this.velocidadX = 0
+        this.velocidadY = 0
     }
 }
 
@@ -117,15 +126,7 @@ function seleccionarMascotaJugador() {
 
     //sectionSeleccionarAtaque.style.display = "flex"
     sectionVerMapa.style.display = "flex"
-    let imagenDeCapipepo = new Image()
-    imagenDeCapipepo.src = capipepo.foto
-    lienzo.drawImage(
-        imagenDeCapipepo,
-        20,
-        40,
-        100,
-        100
-    )
+    intervalo = setInterval(pintarPersonaje, 50)
 
     if (inputHipodoge.checked) {
         spanMascotaJugador.innerHTML = inputHipodoge.id
@@ -304,6 +305,44 @@ function reiniciarJuego() {
 
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function pintarPersonaje() {
+    capipepo.x = capipepo.x + capipepo.velocidadX
+    capipepo.y = capipepo.y + capipepo.velocidadY
+    lienzo.clearRect(0, 0, mapa.width, mapa.height)
+    lienzo.drawImage(
+        capipepo.mapaFoto,
+        capipepo.x,
+        capipepo.y,
+        capipepo.ancho,
+        capipepo.alto
+    )
+}
+
+function moverDerecha() {
+    capipepo.velocidadX = 5
+    
+}
+
+function moverIzquierda() {
+    capipepo,velocidadX = -5
+    
+}
+
+function moverAbajo() {
+    capipepo.velocidadY = 5
+    
+}
+
+function moverArriba() {
+    capipepo.velocidadY = -5
+    
+}
+
+function detenerMovimiento() {
+    capipepo.velocidadX = 0
+    capipepo.velocidadY = 0
 }
 
 window.addEventListener("load", iniciarJuego)
